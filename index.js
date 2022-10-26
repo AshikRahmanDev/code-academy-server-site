6506bf34a7f92f9bba9b31d7a1b200f2c7c8102d
MohammdAshik <mohammadashik797@gmail.com>
2022-10-26 12:10:32 +0600

    send courses data to response for /courses link and deploy to vercel

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 5000;
 
+app.use(cors());
+
+const courses = require("./data/courses.json");
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
 
 app.listen(port, () => {
